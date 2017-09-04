9a0056a60db565c0136d07fa6571a882f2b2b57f
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-09-04 19:09:34 +0200

    Create event when not detected on Google calendar

--- MeetupToGoogle/MeetupToGoogle.fs
+++ MeetupToGoogle/MeetupToGoogle.fs
@@ -9,7 +9,6 @@ let getEvents () =
     let calendar = Ical.Net.Calendar.LoadFromStream(response.ResponseStream)
     calendar 
     |> Seq.collect (fun c -> c.Events)
-    |> Seq.iter (fun e -> printfn "%s" e.Description)
 
 open Google.Apis.Services
 open Google.Apis.Calendar.v3
@@ -28,26 +27,84 @@ let getCredential () =
             Scopes = [CalendarService.Scope.Calendar])
     ServiceAccountCredential(serviceAccountCredential.FromCertificate(certificate))
 
-let baseClientService = BaseClientService.Initializer(
-                            HttpClientInitializer = getCredential(),
-                            ApplicationName = "MeetupToGoogle")
-let calendarService = new CalendarService(baseClientService)
+let getGoogleCalendarService () =
+    let baseClientService = BaseClientService.Initializer(
+                                HttpClientInitializer = getCredential(),
+                                ApplicationName = "MeetupToGoogle")
+    new CalendarService(baseClientService)
 
-let calendarId = "8hc5n2800f4paesicf2u8610d4@group.calendar.google.com" 
+// let e = Data.Event(
+//             Summary = "Youhou!!",
+//             Start = Data.EventDateTime(DateTime = Nullable(DateTime.Now)),
+//             End = Data.EventDateTime(DateTime = Nullable(DateTime.Now.AddHours(2.0))))
+// calendarService.Events.Insert(e, calendarId).Execute()
 
-let e = Data.Event(
-            Summary = "Youhou!!",
-            Start = Data.EventDateTime(DateTime = Nullable(DateTime.Now)),
-            End = Data.EventDateTime(DateTime = Nullable(DateTime.Now.AddHours(2.0))))
-calendarService.Events.Insert(e, calendarId).Execute()
+// let eventsRequest = calendarService.Events.List(
+//                         calendarId, 
+//                         OrderBy = Nullable(EventsResource.ListRequest.OrderByEnum.StartTime),
+//                         TimeMin = Nullable(DateTime.Now))
+// let events = eventsRequest.Execute()
 
-let eventsRequest = calendarService.Events.List(
-                        calendarId, 
-                        OrderBy = Nullable(EventsResource.ListRequest.OrderByEnum.StartTime),
-                        TimeMin = Nullable(DateTime.Now))
-let events = eventsRequest.Execute()
+
+open System.Collections.Generic
+open Ical.Net.Interfaces.Components
+
+type SyncAction = Create of Event | Update of Event
+and Event = {
+    Id: EventId
+    Summary: string
+    Description: string
+    Start: DateTime
+    End: DateTime
+    Updated: DateTime
+}
+and EventId = string
+
+let isSourceEventMoreRecentThanGoogleEventIfExist (existingGoogleEvents:IDictionary<string, Data.Event>) (sourceEvent:IEvent) =
+    existingGoogleEvents.ContainsKey(sourceEvent.Uid) 
+        && sourceEvent.LastModified.Value
+            .CompareTo(existingGoogleEvents.Item(sourceEvent.Uid).Updated.Value) >= 0
+
+let syncEvent (existingGoogleEvents:IDictionary<string, Data.Event>) (event:IEvent) = 
+    let e = { Id = event.Uid
+              Summary = event.Summary
+              Description = event.Description
+              Start = event.Start.Value
+              End = event.End.Value
+              Updated = event.LastModified.Value } 
+    if isSourceEventMoreRecentThanGoogleEventIfExist existingGoogleEvents event then
+        Update e
+    else
+        Create e
+
+let applySync (calendarService:CalendarService) calendarId syncAction =
+    match syncAction with
+    | Create e -> 
+        let googleEvent = Data.Event(
+                            ICalUID = e.Id,
+                            Summary = e.Summary,
+                            Description = e.Description,
+                            Start = Data.EventDateTime(DateTime = Nullable(e.Start)),
+                            End = Data.EventDateTime(DateTime = Nullable(e.End)),
+                            Updated = Nullable(e.Updated))
+        calendarService.Events.Insert(googleEvent, calendarId).Execute() |> ignore 
+    | Update e -> ()
+    syncAction
 
 [<EntryPoint>]
 let main argv =
-    events.Items |> Seq.iter (fun e -> printfn "%A" e.Summary)
+    let calendarId = "8hc5n2800f4paesicf2u8610d4@group.calendar.google.com" 
+    let calendarService = getGoogleCalendarService()
+    let existingGoogleEvents = 
+        calendarService.Events.List(
+            calendarId, 
+            MaxResults = Nullable(1000),
+            TimeMin = Nullable(DateTime.Now)).Execute().Items
+        |> Seq.map (fun e -> e.ICalUID, e)
+        |> dict
+    getEvents()
+    |> Seq.map (syncEvent existingGoogleEvents)
+    |> Seq.map (applySync calendarService calendarId)
+    |> Seq.countBy (function | Create _ -> "created" | Update _ -> "updated")
+    |> Seq.iter (fun x -> printfn "%i %s" (snd x) (fst x))
     0 // return an integer exit code
